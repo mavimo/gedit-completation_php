7f30a7903b3be891fa111544a2801d3414ae694b
Marco Vito Moscaritolo <mavimo@gmail.com>
2009-01-07 15:50:53 +0100

    Added some comment to undestand functionality

--- completion_php.py
+++ completion_php.py
@@ -157,27 +157,33 @@ class CompletionPHPPlugin(gedit.Plugin):
     # TODO: Are these sane defaults? Do we need a configuration dialog?
     _scan_frequency = 1000000000000 # ms
     _max_completions_to_show = 10
-    _function_definition_file = "./phpfunc.txt"
+    _function_definition_file = "/home/marco/Scrivania/phpfunc.txt"
 
     def __init__(self):
-
-        gedit.Plugin.__init__(self)
-        self._all_words = {} # 'function 1', 'function 2', 'other func 1', 'other func 2'}
-        self._completion_windows = {}
-        self._completions = []
-        self._favorite_words = {}
+        gedit.Plugin.__init__(self) # Init this plugin
+        self._all_words = {}        # Hash with all aviable element aviable to display into popup
+        self._completions = []      # Aviable element visible into popup
+        self._favorite_words = {}   # Favourite element added
+        self._remains = []          # Not displayed elements
         self._font_ascent = 0
-        self._remains = []
+        self._completion_windows = {}
 
     def _complete_current(self):
         """Complete the current word."""
 
+        # Load current window
         window = gedit.app_get_default().get_active_window()
+        # Load current document
         doc = window.get_active_document()
+        # Index of completation windows
         index = self._completion_windows[window].get_selected()
+        # Insert into documente (at cursor position), selected text
         doc.insert_at_cursor(self._remains[index])
+        # Set favourites words
         words = self._favorite_words.setdefault(doc, set(()))
+        # Increse into favourites words counter array inserted word
         words.add(self._completions[index])
+        # terminate completation
         self._terminate_completion()
 
     def _connect_document(self, doc):
@@ -220,18 +226,30 @@ class CompletionPHPPlugin(gedit.Plugin):
     def _find_completions(self, doc, incomplete):
         """Find completions for incomplete word and save them."""
 
+        # Empty element array storage
         self._completions = []
         self._remains = []
+        # Load favourites words into temp variable
         favorites = self._favorite_words.get(doc, ())
+        # Create empty array for words
         _all_words = set(())
+        # Cycle every word into aviable words
         for words in self._all_words.itervalues():
+            # Add this eleemnt into temp array
             _all_words.update(words)
+        # Find limit of element to display
         limit = self._max_completions_to_show
+        # Find words ito favourites and all words, limit by 'limit'
         for sequence in (favorites, _all_words):
+            # Cilye sequence of aviable words
             for word in sequence:
+                # check if not start with 'incomplete' text
                 if not word.startswith(incomplete): continue
+                # Check if 'incomplete' is fulltext
                 if word == incomplete: continue
+                # check if term is already aviable into '_completions'
                 if word in self._completions: continue
+                # add term into '_completions'
                 self._completions.append(word)
                 self._remains.append(word[len(incomplete):])
                 if len(self._remains) >= limit: break
